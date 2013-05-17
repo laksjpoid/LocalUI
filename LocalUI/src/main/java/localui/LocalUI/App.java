5cb2c60df63b39ff2e22ec477e1c71ac290e31da
laksjpoid <mengtao09@foxmail.com>
2013-05-17 21:19:28 +0800

    new maven project

---
+++ LocalUI/src/main/java/localui/LocalUI/App.java
@@ -0,0 +1,13 @@
+package localui.LocalUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
