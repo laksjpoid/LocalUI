e5387d8f005e04834a35914c07d2a33b0dd382d5
laksjpoid <mengtao09@foxmail.com>
2013-05-17 23:29:55 +0800

    added swt dependency

--- LocalUI/src/main/java/localui/LocalUI/App.java
+++ LocalUI/src/main/java/localui/LocalUI/App.java
@@ -1,5 +1,12 @@
 package localui.LocalUI;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Hello World");
+        shell.setBounds(100,100,200,50);
+        shell.setLayout(new FillLayout());
+        Label label = new Label(shell, SWT.CENTER);
+        label.setText("Hello World");
+        Color red = new Color(display, 255, 0, 0);
+        label.setForeground(red);
+        shell.open();
+        while(!shell.isDisposed()){
+        	if(!display.readAndDispatch())
+        		display.sleep();
+        }
     }
 }
